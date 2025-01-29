f53344ed8bc002d879b1bf8359e34128fa4fdce5
Alexis <alexis.ramirez.703@mi.unc.edu.ar>
2025-01-29 12:26:52 -0300

    Adding the methods of the player class

--- internal_logic.py
+++ internal_logic.py
@@ -6,7 +6,10 @@ where this structure allows you to go through the song directory forwards and ba
 import os
 from collections import deque
 import random
-from nava import play
+from pydub import AudioSegment
+from pydub.playback import play
+import threading
+import time
 
 def create_songs_queue(directory):
     queue_songs = deque()
@@ -26,14 +29,80 @@ def create_songs_queue(directory):
 
 class Reproductor:
     def __init__(self, songs):
-        self._pause = False
-        self._next_song = False
-        self._back_song = 0 #If 0 == normal, 1 == repeat the current song, 2 == back to the past song
-        self._songs = songs
-        self._current_song = None
-        self._mode = False #If true, activate the random reproduction
+        self.pause = threading.Event()
+        self.next_song = False
+        self.back_song = 0 #If 0 == normal, 1 == repeat the current song, 2 == back to the past song
+        self.songs = songs
+        self.current_song = None
+        self.mode = False #If true, activate the random reproduction
+        self.thread = None
 
-    #Later make the methods for the class 
+    def play_song(self):
+        #Now start in other thread the reproduction
+        if self.thread and self.thread.is_alive():
+            return #Cannot allow more reproductions
+        
+        self.pause.clear() #If the reproduction is paused, we restart
+        self.thread = threading.Thread(target=self._play_loop, daemon=True)
+        self.thread.start()
+
+    #This function change the reproduction mode and control the pause
+    def _play_loop(self):
+        while not self.pause.is_set():
+            if not self.songs:
+                print("Don't have song to reproduction.\n")
+                break
+
+        #If the mode of reproduction are changed
+        if self.mode:
+            self.current_song = random.choice(list(self.songs))
+            sound = AudioSegment.from_mp3(self.current_song)
+        else:
+            self.current_song = self.songs[0]
+            sound = AudioSegment.from_mp3(self.current_song)
+        
+        #Now this is  the loop to management the reproduction
+        start = 0
+        while start < len(sound):
+            #If pause is activeated, we wait the flag
+            self.pause.wait()
+            chunk = sound[start: start+1000]
+            play(chunk)
+            start += 1000
+        self.next_song(self)
+        
+    
+                
+    #Change the mode in normal reproduction to shuffle reproduction
+    def change_mode(self):
+        self.mode = True
+        return self.mode
+
+    #Change the flag to stop the reproduction
+    def stop_reproduction(self):
+        if not self.pause.is_set():
+            self.pause.set()
+        else:
+            self.pause.clear()
+
+    #Rotate to the left the list of the songs 
+    def next_song(self):
+        self.songs.rotate(-1)
+        print(f"{self.songs}")
+        self.current_song = self.songs[0]
+        print(f"{self.current_song}")
+
+    #Rotate to the right the list of the songs if the counter is two
+    def back_song(self):
+        self.back_song +=1 
+        if self.back_song >= 2:
+            self.songs.rotate(1)
+            self.current_song = self.songs[0]
+        return self.back_song
+
+
+
+
 
     
         
